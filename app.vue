84f75e30cc5982efc78e9dfc48f9d6043d0e510b
MadhushaPrasad <madushaprasad21@gmail.com>
2023-10-30 11:13:50 +0530

    feat(component): footer section done

--- app.vue
+++ app.vue
@@ -4,3 +4,15 @@
   </div>
 </template>
 <script lang="ts" setup></script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  width: 100vh;
+  height: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,6 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <h1>Hello Guys! Let's begin to develop</h1>
-  </div>
+  <Footer />
 </template>
-
-<style scoped>
-div {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  height: 100vh;
-}
-
-h1 {
-  margin: 0;
-  height: fit-content;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-transform: uppercase;
-}
-</style>
+<style scoped></style>
